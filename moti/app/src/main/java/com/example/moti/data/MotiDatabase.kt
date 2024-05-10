6ee72356d1b5a5d2c315ac8c883122e85f8c1e18
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-05-10 23:17:58 +0900

    :sparkles: Database

---
+++ moti/app/src/main/java/com/example/moti/data/MotiDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.moti.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.moti.data.dao.AlarmDao
+import com.example.moti.data.dao.TagDao
+import com.example.moti.data.entity.Alarm
+import com.example.moti.data.entity.AlarmAndTag
+import com.example.moti.data.entity.Location
+import com.example.moti.data.entity.RecentLocation
+import com.example.moti.data.entity.Tag
+
+@Database(entities = [Alarm::class, AlarmAndTag::class, RecentLocation::class, Tag::class],
+    version = 1)
+abstract class MotiDatabase :RoomDatabase(){
+    abstract fun alarmDao():AlarmDao
+    abstract fun recentLocationDao():RecentLocation
+    abstract fun tagDao():TagDao
+    companion object{
+        private var instance: MotiDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): MotiDatabase?{
+            if(instance==null){
+                synchronized(MotiDatabase::class){
+                    instance= Room.databaseBuilder(
+                        context.applicationContext,
+                        MotiDatabase::class.java,
+                        "moti-database"
+                    ).fallbackToDestructiveMigration().build()
+                }
+            }
+            return instance
+        }
+    }
+}
