110dffcf43910c0333057d2f887f9737ae5e5b28
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-05-12 02:54:46 +0900

    Feat : 임시 recentLocationRepository

---
+++ moti/app/src/main/java/com/example/moti/data/repository/RecentLocationRepository.kt
@@ -0,0 +1,18 @@
+package com.example.moti.data.repository
+
+class RecentLocationRepository {
+    /**검색 기록 추가*/
+
+    /**검색 기록 업데이트*/
+
+    /**검색 기록 삭제*/
+
+    /**최근 검색 10개 조회*/
+
+    /**장소 저장 추가 (10개 제한)*/
+
+    /**저장한 장소 삭제*/
+
+    /**저장한 장소 전체 조회*/
+
+}
