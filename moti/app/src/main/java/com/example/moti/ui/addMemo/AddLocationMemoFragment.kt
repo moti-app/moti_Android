2281ca99faab81c1062dd16a5e48883b8e7b4e2a
seokmin <14891@naver.com>
2024-05-15 19:30:52 +0900

    Added event listener on AddLocationMemoFragment
    - locationTitleTextView -> locationTitleEditText
    - radius in not implemented
    - Notification type setting, tag setting not implemented

--- moti/app/src/main/java/com/example/moti/ui/addMemo/AddLocationMemoFragment.kt
+++ moti/app/src/main/java/com/example/moti/ui/addMemo/AddLocationMemoFragment.kt
@@ -1,17 +1,22 @@
 package com.example.moti.ui.map
 
 import android.app.Dialog
-import android.location.Address
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.moti.R
+import com.example.moti.data.MotiDatabase
+import com.example.moti.data.entity.Alarm
 import com.example.moti.data.entity.Location
 import com.example.moti.data.entity.Week
+import com.example.moti.data.repository.AlarmRepository
 import com.example.moti.databinding.FragmentAddMemoBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AddLocationMemoFragment : BottomSheetDialogFragment() {
     private var name: String = "noname"
@@ -25,9 +30,12 @@ class AddLocationMemoFragment : BottomSheetDialogFragment() {
     private var location : Location = Location(lat,lng,address,name)
 
     private var radius : Double = 0.0
-    private var isRepeat : Boolean = false
+    private var isRepeat : Boolean = true
     private var repeatDay : Week = Week.MON
     private var hasBanner : Boolean = true
+
+    private lateinit var db:MotiDatabase
+    private lateinit var alarmRepository: AlarmRepository
     companion object {
         private const val ARG_NAME = "name"
         private const val ARG_LAT = "lat"
@@ -60,6 +68,11 @@ class AddLocationMemoFragment : BottomSheetDialogFragment() {
             lng = it.getDouble(ARG_LNG, 0.0)
             address = it.getString(ARG_addr) ?: "noname"
         }
+        db = MotiDatabase.getInstance(requireActivity().applicationContext)!!
+        alarmRepository = AlarmRepository(db.alarmDao(),db.tagDao(),db.alarmAndTagDao())
+
+
+
     }
 
     override fun onCreateView(
@@ -72,10 +85,56 @@ class AddLocationMemoFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Setup your UI and logic here
-//        Toast.makeText(activity, name,Toast.LENGTH_SHORT).show()
-        binding.locationTitleTextView.text = name
+        binding.saveCancelBtn.setOnClickListener() {
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
+        binding.locationTitleEditText.setText(name)
         binding.locationDetailTextView.text = address
+        binding.inOrOutRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
+            when(i) {
+                binding.inRadioBtn.id->whenArrival = true
+                binding.outRadioBtn.id->whenArrival = false
+            }
+        }
+        // TODO: 반복 요일 구현 (repeatDay)
+        binding.repeatSwitch.setOnCheckedChangeListener { compoundButton, b ->
+            if (b) {
+                isRepeat = true
+            }
+            else {
+                isRepeat = false
+            }
+        }
+        binding.alarmTypeLinearLayout.setOnClickListener() {
+            // TODO: 알림 유형 구현
+        }
+        binding.tagLinearLayout.setOnClickListener() {
+            // TODO: 태그 구현
+        }
+        // TODO: 반경 구현
+
+        binding.saveBtn.setOnClickListener() {
+            location = Location(
+                lat,lng,address,name
+            )
+            context = binding.memoEditText.text.toString()
+            val alarm = Alarm(
+                title = name,
+                context = context,
+                location = location,
+                whenArrival = whenArrival,
+                radius = radius,
+                isRepeat = isRepeat,
+                repeatDay = repeatDay,
+                hasBanner = hasBanner
+            )
+            val list: List<Long> = listOf() // TODO: 태그 구현
+
+            CoroutineScope(Dispatchers.IO).launch {
+                alarmRepository.createAlarmAndTag(alarm, tagIds = list)
+            }
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
