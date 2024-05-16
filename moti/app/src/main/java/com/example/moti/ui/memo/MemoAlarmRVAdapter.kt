3a696a122ea5fc8192dc7354daca3ca694cea984
Hyejeong <myejeong0211@gmail.com>
2024-05-16 21:58:21 +0900

    Feat: Alarm DB-repeatDay 연결

--- moti/app/src/main/java/com/example/moti/ui/memo/MemoAlarmRVAdapter.kt
+++ moti/app/src/main/java/com/example/moti/ui/memo/MemoAlarmRVAdapter.kt
@@ -7,9 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moti.R
 import com.example.moti.data.MotiDatabase
 import com.example.moti.data.entity.Alarm
+import com.example.moti.data.entity.Week
 import com.example.moti.data.repository.AlarmRepository
 import com.example.moti.databinding.ItemMemoAlarmBinding
 
@@ -32,7 +35,24 @@ class MemoAlarmRVAdapter(private val context: Context, private val alarmList: Li
         holder.binding.itemMemoPlaceTv.text = alarmList[position].title
         holder.binding.itemMemoAddressTv.text = alarmList[position].location.address
 
+        val selectColor = ContextCompat.getColor(holder.itemView.context, R.color.mt_main)
 
+        val dayToViewsMap = mapOf(
+            Week.SUN to Pair(holder.binding.itemMemoSunTv, holder.binding.itemMemoDot1Iv),
+            Week.MON to Pair(holder.binding.itemMemoMonTv, holder.binding.itemMemoDot2Iv),
+            Week.TUE to Pair(holder.binding.itemMemoTueTv, holder.binding.itemMemoDot3Iv),
+            Week.WED to Pair(holder.binding.itemMemoWedTv, holder.binding.itemMemoDot4Iv),
+            Week.THU to Pair(holder.binding.itemMemoThuTv, holder.binding.itemMemoDot5Iv),
+            Week.FRI to Pair(holder.binding.itemMemoFriTv, holder.binding.itemMemoDot6Iv),
+            Week.SAT to Pair(holder.binding.itemMemoSatTv, holder.binding.itemMemoDot7Iv)
+        )
+
+        val views = dayToViewsMap[alarmList[position].repeatDay]
+
+        if (views != null) {
+            views.first.setTextColor(selectColor)
+            views.second.setColorFilter(selectColor)
+        }
         val toggle = holder.binding.itemMemoToggleSc
 
         var checked = false
