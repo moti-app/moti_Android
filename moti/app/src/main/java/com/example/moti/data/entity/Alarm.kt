da5ca5f03edaea68d279d9f1ec3875b089658c5d
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-05-21 19:14:28 +0900

    :sparkles: Feat : DB업데이트
    <body>
    - 태그 색 필드 추가
    - 알림 테이블에 필드 추가
        - 마지막 알림 울린 시간 : DateTime
        - 알림 다시 울리기 간격 : Int (기본값 24시간)
    - 반복일 때 요일 여러 개 선택

--- moti/app/src/main/java/com/example/moti/data/entity/Alarm.kt
+++ moti/app/src/main/java/com/example/moti/data/entity/Alarm.kt
@@ -3,6 +3,7 @@ package com.example.moti.data.entity
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDateTime
 
 @Entity(tableName = "Alarm")
 data class Alarm (
@@ -15,16 +16,24 @@ data class Alarm (
     var whenArrival : Boolean,
     var radius : Double,
     var isRepeat : Boolean,
-    var repeatDay : Week,
-    var hasBanner : Boolean
+    var repeatDay : List<Week>?,
+    var hasBanner : Boolean,
+    var tagColor : TagColor,
+    var lastNoti : LocalDateTime,
+    var interval : Int = 1440
 ){
-  constructor(title : String,
-              context : String,
-              location : Location,
-              whenArrival : Boolean,
-              radius : Double,
-              isRepeat : Boolean,
-              repeatDay : Week,
-              hasBanner : Boolean)
-          : this(0, title, context, location, whenArrival, radius, isRepeat, repeatDay, hasBanner)
+  constructor(
+      title : String,
+      context : String,
+      location : Location,
+      whenArrival : Boolean,
+      radius : Double,
+      isRepeat : Boolean,
+      repeatDay : List<Week>?,
+      hasBanner : Boolean,
+      tagColor: TagColor,
+      lastNoti : LocalDateTime,
+      interval : Int)
+          : this(0, title, context, location, whenArrival, radius, isRepeat,
+      repeatDay, hasBanner, tagColor, lastNoti, interval)
 }
