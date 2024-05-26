f11ee1cf4b71ebc8e27c7040a2cf51dc6585625e
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-05-26 18:36:00 +0900

    :sparkles: Feat : 메모 이미지 UI
    메모이미지~태그까지 스크롤 되도록 설정

--- moti/app/src/main/java/com/example/moti/ui/addMemo/AddLocationMemoFragment.kt
+++ moti/app/src/main/java/com/example/moti/ui/addMemo/AddLocationMemoFragment.kt
@@ -14,6 +14,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -373,6 +374,11 @@ class AddLocationMemoFragment : BottomSheetDialogFragment(),
             "$radius m"
         } else {
             String.format("%.1f km", radius / 1000.0)
+        }
+        //부모 스크롤 막기
+        binding.innerScroll.setOnTouchListener { view, motionEvent ->
+            binding.scrollView.requestDisallowInterceptTouchEvent(true)
+            false
         }
     }
 
